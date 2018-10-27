7834a08e00226ffd8b9ce778c4e16ffffb8a4359
jafeer <jafeer1990@gmail.com>
2018-10-27 23:19:39 +0400

    2nd usercommit

--- src/test/java/jnjsfbsf.java
+++ src/test/java/jnjsfbsf.java
@@ -4,6 +4,11 @@ public class jnjsfbsf {
 	private void syso() {
 		// TODO Auto-generated method stub
    System.out.println("jafeer sis mass");
+   
+   System.out.println("kjs nfksgsgsg");
+   
+   System.out.println("kjs sfdgsgsg");
+   
     
 	}
 
